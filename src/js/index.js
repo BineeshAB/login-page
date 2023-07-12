e12675bf0d0fc2731c39ad69162bbca52d90f7f4
BineeshAB <bineeshalakkal22@gmail.com>
2023-07-12 22:03:02 +0530

    Create a Sixth Project (Login Form), using HTML & CSS

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+// let mailLabel = document.getElementById("mail-label");
+// let pwdLabel = document.getElementById("pwd-label");
+
+// let mailValue = document.getElementById("mail");
+// let pwdValue = document.getElementById("pwd");
+
+// let mailText = mailLabel.innerText.split("");
+// let pwdText = pwdLabel.innerText.split("");
+
+// if (mailValue.focus()) {
+//     mailLabel.style.top = "-30px";
+// }
